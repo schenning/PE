ea13d9ae6c3dff86120cd0c9891faf2a5c3466d0
Henning <henninsc@stud.ntnu.no>
2015-10-05 10:57:02 +0200

    Problem 2

---
+++ pb2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+/* Problem 2: PE */
+/* Written by Schenning */
+int main() {
+
+	int  res=0,a=1,b=1,tmp=0;
+	while (tmp < 4*pow(10,6))
+		{	
+			tmp = a+b;
+			a   = b;		
+			b   = tmp;
+			if (tmp%2==0)
+			{
+				res+=tmp;		
+			}	
+		}
+	printf("%d\n", res);
+
+return 0;
+
+}
--- pb2.py
+++ pb2.py
@@ -1,29 +1,14 @@
-def trial_division(n):
-    
-    if n == 1: return [1]
-    primes = prime_sieve(int(n**0.5) + 1)
-    prime_factors = []
- 
-    for p in primes:
-        if p*p > n: break
-        while n % p == 0:
-            prime_factors.append(p)
-            n /= p
-    if n > 1: prime_factors.append(n)
-
- 
-    return prime_factors
+# Problem 2: PE
+# written by Schenning
 
 
-def prime_sieve(n):
-    size = n//2
-    sieve = [1]*size
-    limit = int(n**0.5)
-    for i in range(1,limit):
-        if sieve[i]:
-            val = 2*i+1
-            tmp = ((size-1) - i)//val 
-            sieve[i+val::val] = [0]*tmp
-    return sieve
+res = 0
+a,b=1,1
+while a < 4*10**6:
+    a,b =b, a+b
+    if a%2==0:
+        res+=a
 
-print trial_division(55)
+print res
+
+
